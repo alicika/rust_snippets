157a3756fbccfd9ee6b440749b40e0910f1dad30
alicika <52126032+alicika@users.noreply.github.com>
2021-04-26 01:27:49 +0900

    extend user-defined errors and their implementation

--- etf_capital_profit_calc/src/error.rs
+++ etf_capital_profit_calc/src/error.rs
@@ -1,7 +1,9 @@
 use std::fmt;
+use std::fs::File;
+use std::io;
 
 struct AppError {
-    code: usize,
+    kind: String,
     message: String,
 }
 
@@ -26,24 +28,35 @@ impl fmt::Debug for AppError {
         )
     }
 }
-
-fn produce_error() -> Result<(), AppError> {
-    Err(AppError {
-        code: 404,
-        message: String::from("Page not found"),
-    })
+impl From<io::Error> for AppError {
+    fn from(error: io::Error) -> Self {
+        AppError {
+            kind: String::from("io"),
+            message: error.to_string(),
+        }
+    }
 }
 
-fn main() {
-    match produce_error() {
-        Err(e) => eprintln!("{}", e), // Sorry, Can not find the Page!
-        _ => println!("No error"),
+// Implement std::convert::From for AppError; from num::ParseIntError
+impl From<num::ParseIntError> for AppError {
+    fn from(error: num::ParseIntError) -> Self {
+        AppError {
+            kind: String::from("parse"),
+            message: error.to_string(),
+        }
     }
+}
 
-    eprintln!("{:?}", produce_error()); // Err(AppError { code: 404, message: Page not found })
 
-    eprintln!("{:#?}", produce_error());
-    // Err(
-    //     AppError { code: 404, message: Page not found }
-    // )
+// example use to
+fn main() -> Result<(), AppError> {
+    let mut file = File::open("no.txt")?; // generates an io::Error, if can not open the file and converts to an AppError
+
+    let mut content = String::new();
+    file.read_to_string(&mut content)?; // generates an io::Error, if can not read file content and converts to an AppError
+
+    let _number: usize;
+    _number = content.parse()?; // generates num::ParseIntError, if can not convert file content to usize and converts to an AppError
+
+    Ok(())
 }
