0efe9be25e85c4652b3c0db93b21e907626104ee
alicika <52126032+alicika@users.noreply.github.com>
2021-08-20 23:59:31 +0900

    add a port number returning function

---
+++ portscanner/target/debug/deps/portscanner-6784475981466316.d
@@ -0,0 +1,5 @@
+C:\Users\PYLP\default\rust_snippet\portscanner\target\debug\deps\portscanner-6784475981466316.exe: src\main.rs
+
+C:\Users\PYLP\default\rust_snippet\portscanner\target\debug\deps\portscanner-6784475981466316.d: src\main.rs
+
+src\main.rs:
