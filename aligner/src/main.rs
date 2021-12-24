f37bac784129bc4759f20051c7c06f4e1dca493a
alicika <52126032+alicika@users.noreply.github.com>
2021-12-24 23:57:37 +0900

    making a quick fix on tests

--- aligner/src/main.rs
+++ aligner/src/main.rs
@@ -9,18 +9,19 @@ pub fn increasing<'a>(slice: &'a [i32]) -> &'a [i32] {
     ret
 }
 
-fn main() {
+fn main() -> Result<(), String> {
     let vec = vec![2, 4, 7, 8, 6, 3, 5];
     let result;
     {
         let slice = &vec[..];
         result = increasing(slice);
     }
+    assert_eq!(result, &[2, 3, 4, 5, 6, 7, 8]);
+    Ok(())
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod test {
     #[test]
-    assert_eq!(result, &[2, 4, 7, 8]);
+    assert_eq!(increasing(&[8, 4, 2, 7]), &[2, 4, 7, 8]);
 }
