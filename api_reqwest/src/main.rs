a48ab8f6fdd60cda9bd8c77d84231a1ffd997b70
alicika <52126032+alicika@users.noreply.github.com>
2021-12-26 23:39:08 +0900

    configure an async tokio function

--- api_reqwest/src/main.rs
+++ api_reqwest/src/main.rs
@@ -1,6 +1,15 @@
 use reqwest;
 use tokio::net::TcpStream;
 
-fn main() {
-    
+use std::collections::HashMap;
+
+#[tokio::main]
+async fn main() {
+    let t = reqwest::Client::builder().build()?;
+    println!("{:?}", t);
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp)
 }
