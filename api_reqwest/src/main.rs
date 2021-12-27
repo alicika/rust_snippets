a889be01422fc01bafbca4aa41ba48d42a8063e6
alicika <52126032+alicika@users.noreply.github.com>
2021-12-27 23:48:35 +0900

    make changes in order to use reqwest::Client

--- api_reqwest/src/main.rs
+++ api_reqwest/src/main.rs
@@ -4,12 +4,14 @@ use tokio::net::TcpStream;
 use std::collections::HashMap;
 
 #[tokio::main]
-async fn main() {
-    let t = reqwest::Client::builder().build()?;
-    println!("{:?}", t);
-    let resp = reqwest::get("https://httpbin.org/ip")
+async fn main() -> Result<(), reqwest::Error> {
+    let t = reqwest::Client::new();
+    
+    let resp = t.get("https://httpbin.org/ip")
         .await?
         .json::<HashMap<String, String>>()
         .await?;
-    println!("{:#?}", resp)
+    println!("{:#?}", resp);
+
+    Ok(())
 }
