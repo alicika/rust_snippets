9f217b0230f7cb976cf8d3dc43c15777ec984647
alicika <52126032+alicika@users.noreply.github.com>
2021-12-29 23:44:26 +0900

    implementing structs

--- api_reqwest/src/main.rs
+++ api_reqwest/src/main.rs
@@ -1,13 +1,25 @@
 use reqwest;
 use tokio;
-
+use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[allow(dead_code)]
+struct TweetBody<'a> {
+    txt: &'a str
+}
+
+#[allow(dead_code)]
+struct Tweets<'a> {
+    field: Vec<TweetBody<'a>>
+}
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let t = reqwest::Client::new();
-    
-    let resp = t.get("https://api.twitter.com/1.1/search/tweets.json")
+
+    let resp = t
+        .get("https://api.twitter.com/1.1/search/tweets.json")
         .send()
         .await?
         .json::<HashMap<String, String>>()
