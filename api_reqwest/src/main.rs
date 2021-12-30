f174c283b2e5d74d7da5a873d4c5c9dab792909b
alicika <52126032+alicika@users.noreply.github.com>
2021-12-30 18:28:57 +0900

    adding functionarity to send a request

--- api_reqwest/src/main.rs
+++ api_reqwest/src/main.rs
@@ -1,25 +1,29 @@
 use reqwest;
-use tokio;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use tokio;
 
 #[derive(Serialize, Deserialize, Debug)]
 #[allow(dead_code)]
 struct TweetBody<'a> {
-    txt: &'a str
-}
-
-#[allow(dead_code)]
-struct Tweets<'a> {
-    field: Vec<TweetBody<'a>>
+    txt: &'a str,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let t = reqwest::Client::new();
 
+    let v = std::env::args().nth(1);
+
+    // if there is a searching query given as the first argument,
+    // set it as a parameter
+    let url = match v {
+        None => "https://api.twitter.com/1.1/search/tweets.json".to_string(),
+        Some(query) => format!("https://api.twitter.com/1.1/search/tweets.json?{}", query),
+    };
+
     let resp = t
-        .get("https://api.twitter.com/1.1/search/tweets.json")
+        .get(url)
         .send()
         .await?
         .json::<HashMap<String, String>>()
