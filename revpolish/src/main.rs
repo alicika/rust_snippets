589123981dc65f694733be22000b70958350d883
alicika <52126032+alicika@users.noreply.github.com>
2022-03-22 23:59:35 +0900

    add anyhow Result to eval

--- revpolish/src/main.rs
+++ revpolish/src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use anyhow::{Context, Result};
+use anyhow::{Context, Result, ensure, bail};
 use std::fs::File;
 use std::io::{stdin, BufRead, BufReader};
 
@@ -25,20 +25,23 @@ impl RPNCalculator {
         Self(verbose)
     }
 
-    pub fn eval(&self, formula: &str) -> i32 {
+    pub fn eval(&self, formula: &str) -> Result<i32> {
         let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
         self.eval_inner(&mut tokens)
     }
 
-    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> Result<i32> {
         let mut stack = Vec::new();
+        let mut pos = 0;
 
         while let Some(token) = tokens.pop() {
+            pos += 1;
+
             if let Ok(x) = token.parse::<i32>() {
                 stack.push(x)
             } else {
-                let y = stack.pop().expect("invalid syntax");
-                let x = stack.pop().expect("invalid syntax");
+                let y = stack.pop().context(format!("invalid syntax at {}", pos))?;
+                let x = stack.pop().context(format!("invalid syntax at {}", pos))?;
 
                 let res = match token {
                     "+" => x + y,
@@ -46,7 +49,7 @@ impl RPNCalculator {
                     "*" => x * y,
                     "/" => x / y,
                     "%" => x % y,
-                    _ => panic!("got an unexpected token"),
+                    _ => bail!("invalid tokens at {}", pos),
                 };
                 stack.push(res)
             }
@@ -55,12 +58,8 @@ impl RPNCalculator {
                 println!("{:?}, {:?}", tokens, stack);
             }
         }
-        if stack.len() == 1 {
-            stack[0]
-        } else {
-            panic!("invalid syntax")
-        }
-    }
+        ensure!(stack.len() == 1, "invalid syntax");
+        Ok(stack[0])
 }
 
 fn main() {
