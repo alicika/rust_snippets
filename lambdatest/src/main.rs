dd693e16ab460e94d3542872c63b3e42bc4715ea
alicika <52126032+alicika@users.noreply.github.com>
2022-03-27 23:59:22 +0900

    add Mutex placeholder

--- lambdatest/src/main.rs
+++ lambdatest/src/main.rs
@@ -8,3 +8,25 @@ async fn main() -> Result<(), actix_web::Error> {
     .await?;
     Ok(())
 }
+
+async fn index(
+    counter_mutex: Data<Mutex<usize>>,
+    counter_cell: Data<Cell<u32>>,
+    counter_atomic: Data<AtomicUsize>,
+    req: HttpRequest,
+) -> HttpResponse {
+    println!("{:?}", req);
+
+    // Increment the counters
+    *counter_mutex.lock().unwrap() += 1;
+    counter_cell.set(counter_cell.get() + 1);
+    counter_atomic.fetch_add(1, Ordering::SeqCst);
+
+    let body = format!(
+        "global mutex counter: {}, local counter: {}, global atomic counter: {}",
+        *counter_mutex.lock().unwrap(),
+        counter_cell.get(),
+        counter_atomic.load(Ordering::SeqCst),
+    );
+    HttpResponse::Ok().body(body)
+}
